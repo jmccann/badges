b1158ca4ef8d0fa277f617d98f0fcd0cfe02c43f
Jacob McCann <jacob.mccann2@target.com>
2016-10-14 11:18:39 +0530

    Hard code some return codes

--- lib/badges/get.rb
+++ lib/badges/get.rb
@@ -16,7 +16,7 @@ end
 #
 get '/badges/:owner' do |owner|
   badges = Badge.all(owner: owner)
-  badges.to_json
+  [200, badges.to_json]
 end
 
 #
@@ -30,7 +30,7 @@ end
 #
 get '/badges/:owner/:project/:name' do |owner, project, name|
   badge = Badge.all(owner: owner, project: project, name: name)
-  return badge.first.to_json if badge.count == 1
+  return [200, badge.first.to_json] if badge.count == 1
   [500, message: "Too many badges returned: #{badge.count}"] if badge.count > 1
 end
 
@@ -40,6 +40,6 @@ end
 get '/badges/:owner/:project/:name/badge.svg' do |owner, project, name|
   content_type 'image/svg+xml'
   badge = Badge.all(owner: owner, project: project, name: name)
-  return badge.first.image if badge.count == 1
+  return [200, badge.first.image] if badge.count == 1
   [500, message: "Too many badges returned: #{badge.count}"] if badge.count > 1
 end
