e9b3457c0280e9f4aac1566c4c3d3fbcb0b68d51
Jacob McCann <jacob.mccann2@target.com>
2017-04-24 15:20:27 -0500

    Refactor get endpoints

--- lib/badges/get.rb
+++ lib/badges/get.rb
@@ -29,34 +29,37 @@ end
 # Show badge details
 #
 get '/badges/:owner/:project/:name' do |owner, project, name|
-  badge = Badge.all(owner: owner, project: project, name: name, branch: nil)
-  return [200, badge.first.to_json] if badge.count == 1
-  [500, message: "Too many badges returned: #{badge.count}"] if badge.count > 1
+  get_badge(owner, project, name, nil)
 end
 
 #
 # Return badge
 #
 get '/badges/:owner/:project/:name/badge.svg' do |owner, project, name|
-  content_type 'image/svg+xml'
-  badge = Badge.all(owner: owner, project: project, name: name, branch: nil)
-  return [200, badge.first.image] if badge.count == 1
-  [500, message: "Too many badges returned: #{badge.count}"] if badge.count > 1
+  get_badge_svg(owner, project, name, nil)
 end
 
 #
 # Show badge details for a branch
 #
 get '/badges/:owner/:project/:name/:branch' do |owner, project, name, branch|
-  badge = Badge.all(owner: owner, project: project, name: name, branch: branch)
-  return [200, badge.first.to_json] if badge.count == 1
-  [500, message: "Too many badges returned: #{badge.count}"] if badge.count > 1
+  get_badge(owner, project, name, branch)
 end
 
 #
 # Return badge for a branch
 #
 get '/badges/:owner/:project/:name/:branch/badge.svg' do |owner, project, name, branch| # rubocop:disable LineLength
+  get_badge_svg(owner, project, name, branch)
+end
+
+def get_badge(owner, project, name, branch = nil)
+  badge = Badge.all(owner: owner, project: project, name: name, branch: branch)
+  return [200, badge.first.to_json] if badge.count == 1
+  [500, message: "Too many badges returned: #{badge.count}"] if badge.count > 1
+end
+
+def get_badge_svg(owner, project, name, branch = nil)
   content_type 'image/svg+xml'
   badge = Badge.all(owner: owner, project: project, name: name, branch: branch)
   return [200, badge.first.image] if badge.count == 1
