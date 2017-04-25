9930c0969d6d1a0d867a865ff225cb5ece9ffc85
Jacob McCann <jacob.mccann2@target.com>
2017-04-25 08:24:40 -0500

    Refactor POST endpoint

--- lib/badges/post.rb
+++ lib/badges/post.rb
@@ -5,21 +5,21 @@ require_relative '../helpers/datamapper'
 require_relative '../helpers/data'
 
 post '/badges/:owner/:project/:name' do |owner, project, name|
-  badge = new_badge(owner, project, name, data)
-
-  # After creating object return details of it
-  redirect "/badges/#{badge.full_name}" if badge.save
-
-  error = badge.errors.full_messages.join(', ')
-  return [500, { message: "Failed to create new Badge: #{error}" }.to_json]
+  post_badge(owner, project, name, data, nil)
 end
 
 post '/badges/:owner/:project/:name/:branch' do |owner, project, name, branch|
+  post_badge(owner, project, name, data, branch)
+end
+
+def post_badge(owner, project, name, data, branch = nil)
   badge = new_badge(owner, project, name, data, branch)
 
   # After creating object return details of it
-  redirect "/badges/#{badge.full_name}/#{badge.branch}" if badge.save
+  url = "/badges/#{badge.full_name}"
+  url += "/#{badge.branch}" unless branch.nil?
+  redirect url if badge.save
 
   error = badge.errors.full_messages.join(', ')
-  return [500, { message: "Failed to create new Badge: #{error}" }.to_json]
+  [500, { message: "Failed to create new Badge: #{error}" }.to_json]
 end
